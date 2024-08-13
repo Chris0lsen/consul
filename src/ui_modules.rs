555de40246aeda0cfb73aec0d0602a601d894b7a
chris0lsen <chris@chrisolsen.net>
2024-08-13 09:50:39 -0500

    Update CI to iclude fmt

--- src/ui_modules.rs
+++ src/ui_modules.rs
@@ -1,3 +1,3 @@
 slint::include_modules!();
-// Include the Slint modules here so they can be reused 
+// Include the Slint modules here so they can be reused
 // with a single reference elsewhere in the application
