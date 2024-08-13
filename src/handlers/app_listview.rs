b67f721ff045825aad0469f5ef62379523e2d66b
ircjwin <christopherjamesirwin@gmail.com>
2024-08-13 10:51:39 -0700

    refactor: remove redundant code
    app_listview.rs:
    - Convert `task_items_model` and `task_items_model_rc` to `items_model` and `items_model_rc`
    - Clone `ui_weak` and remove redeclaration
    - Add comments for clarity
    
    appwindow.slint:
    - Remove zombie code
    - Convert `task_items` to `items`
    - Add comments for clarity

--- src/handlers/app_listview.rs
+++ src/handlers/app_listview.rs
@@ -15,29 +15,25 @@ pub enum AppListviewEvent {
 }
 
 pub fn init(ui: &AppWindow) {
-    let items_model: Rc<VecModel<SharedString>> =
-        Rc::new(VecModel::from(vec!["Hello".into(), "World".into()]));
-    let items_model_rc = ModelRc::from(items_model.clone());
-    ui.set_items(items_model_rc);
-
-    let task_items_model: Rc<VecModel<TaskItem>> =
+    // Initialize AppWindow items with placeholder values
+    let items_model: Rc<VecModel<TaskItem>> =
         Rc::new(VecModel::from(vec![
             TaskItem {title: "Hello".into(), checked: false},
             TaskItem {title: "World".into(), checked: false}]
         ));
-    let task_items_model_rc = ModelRc::from(task_items_model.clone());
-    ui.set_task_items(task_items_model_rc);
+    let items_model_rc = ModelRc::from(items_model.clone());
+    ui.set_items(items_model_rc);
 
-    let ui_weak = ui.as_weak();
+    // Initialize add and remove handlers
+    let ui_weak_1 = ui.as_weak();
+    let ui_weak_2 = ui_weak_1.clone();
     ui.on_request_add_item(move || {
-        let app = ui_weak.unwrap();
+        let app = ui_weak_1.unwrap();
         let input_text = app.get_input_text();
         app.handle_event(AppListviewEvent::AddItem(input_text));
     });
-
-    let ui_weak = ui.as_weak();
     ui.on_request_remove_item(move || {
-        let app = ui_weak.unwrap();
+        let app = ui_weak_2.unwrap();
         app.handle_event(AppListviewEvent::RemoveItem());
     });
 }
@@ -57,41 +53,32 @@ impl AppWindow {
 }
 
 fn handle_add_item(app: &AppWindow, text: SharedString) {
+    // Convert ModelRc to Model for access to Vector methods
     let items_model_rc = app.get_items();
     let items_model = items_model_rc
         .as_any()
-        .downcast_ref::<VecModel<SharedString>>()
-        .expect("We know we set a VecModel earlier");
-    let task_items_model_rc = app.get_task_items();
-    let task_items_model = task_items_model_rc
-        .as_any()
         .downcast_ref::<VecModel<TaskItem>>()
         .expect("We know we set a VecModel earlier");
+
+    // Appends user input and empties TextInput component
     if !text.is_empty() {
-        let task_text = text.clone();
-        items_model.push(SharedString::from(text));
-        task_items_model.push(TaskItem {title: task_text.into(), checked: false});
+        items_model.push(TaskItem {title: text.into(), checked: false});
         app.set_input_text(SharedString::new());
     }
 }
 
 fn handle_remove_item(app: &AppWindow) {
+    // Convert ModelRc to Model for access to Vector methods
     let items_model_rc = app.get_items();
     let items_model = items_model_rc
-        .as_any()
-        .downcast_ref::<VecModel<SharedString>>()
-        .expect("We know we set a VecModel earlier");
-
-    let task_items_model_rc = app.get_task_items();
-    let task_items_model = task_items_model_rc
         .as_any()
         .downcast_ref::<VecModel<TaskItem>>()
         .expect("We know we set a VecModel earlier");
 
+    // Removes checked items and adjusts offset to maintain index order
     let mut offset = 0;
-    for i in 0..task_items_model.row_count() {
-        if task_items_model.row_data(i - offset).unwrap().checked {
-            task_items_model.remove(i - offset);
+    for i in 0..items_model.row_count() {
+        if items_model.row_data(i - offset).unwrap().checked {
             items_model.remove(i - offset);
             offset += 1;
         }
