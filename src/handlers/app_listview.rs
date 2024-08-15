a8a4806bd16498070abd8c033454ad8023e0bdd4
chris0lsen <chris@chrisolsen.net>
2024-08-15 13:19:09 -0500

    refactor to use one arc for weak ui

--- src/handlers/app_listview.rs
+++ src/handlers/app_listview.rs
@@ -1,14 +1,14 @@
 use crate::ui_modules::AppWindow; // Import the re-exported AppWindow type
-// use crate::ui_events::UIEvent; // Import the UIEvent enum
+                                  // use crate::ui_events::UIEvent; // Import the UIEvent enum
 use crate::ui_modules::TaskItem;
+use slint::ComponentHandle;
 use slint::Model;
 use slint::ModelRc;
 use slint::SharedString;
 use slint::VecModel;
-use slint::ComponentHandle;
 use std::rc::Rc;
-use std::thread;
 use std::sync::{Arc, Mutex};
+use std::thread;
 
 pub enum AppListviewEvent {
     AddItem(SharedString),
@@ -18,41 +18,46 @@ pub enum AppListviewEvent {
 
 pub fn init(ui: &AppWindow) {
     // Initialize AppWindow items with placeholder values
-    let items_model: Rc<VecModel<TaskItem>> =
-        Rc::new(VecModel::from(vec![
-            TaskItem {title: "Hello".into(), checked: false},
-            TaskItem {title: "World".into(), checked: false}]
-        ));
+    let items_model: Rc<VecModel<TaskItem>> = Rc::new(VecModel::from(vec![
+        TaskItem {
+            title: "Hello".into(),
+            checked: false,
+        },
+        TaskItem {
+            title: "World".into(),
+            checked: false,
+        },
+    ]));
     let items_model_rc = ModelRc::from(items_model.clone());
     ui.set_items(items_model_rc);
 
-    // Initialize Arc for AddItem handler
-    let ui_weak_1 = Arc::new(Mutex::new(ui.as_weak()));
-    // Clone Arc for RemoveItem handler
-    let ui_weak_2 = Arc::clone(&ui_weak_1);
+    // Initialize Arc to be cloned by each handler
+    let ui_arc = Arc::new(Mutex::new(ui.as_weak()));
 
+    let add_item_handler_arc = Arc::clone(&ui_arc);
     ui.on_request_add_item(move || {
         // Clone Arc for thread
-        let ui_weak_1 = Arc::clone(&ui_weak_1);
+        let local_handler_clone = Arc::clone(&add_item_handler_arc);
         thread::spawn(move || {
             // Locks Weak UI reference until it goes out of scope
-            let ui_weak_1 = ui_weak_1.lock().unwrap();
+            let ui_weak = local_handler_clone.lock().unwrap();
             // Capture unused Result in placeholder variable
-            let _ = ui_weak_1.upgrade_in_event_loop(move |ui| {
+            let _ = ui_weak.upgrade_in_event_loop(move |ui| {
                 let text = ui.get_input_text();
                 ui.handle_event(AppListviewEvent::AddItem(text));
             });
         });
     });
 
+    let remove_item_handler_arc = Arc::clone(&ui_arc);
     ui.on_request_remove_item(move || {
         // Clone Arc for thread
-        let ui_weak_2 = Arc::clone(&ui_weak_2);
+        let local_handler_clone = Arc::clone(&remove_item_handler_arc);
         thread::spawn(move || {
             // Locks Weak UI reference until it goes out of scope
-            let ui_weak_2 = ui_weak_2.lock().unwrap();
+            let ui_weak = local_handler_clone.lock().unwrap();
             // Capture unused Result in placeholder variable
-            let _ = ui_weak_2.upgrade_in_event_loop(move |ui| {
+            let _ = ui_weak.upgrade_in_event_loop(move |ui| {
                 ui.handle_event(AppListviewEvent::RemoveItem());
             });
         });
@@ -62,13 +67,8 @@ pub fn init(ui: &AppWindow) {
 impl AppWindow {
     pub fn handle_event(&self, event: AppListviewEvent) {
         match event {
-            AppListviewEvent::AddItem(text) => {
-               handle_add_item(&self, text)
-            },
-            AppListviewEvent::RemoveItem() => {
-                handle_remove_item(&self)
-            }
-            // Add more event handling here as needed
+            AppListviewEvent::AddItem(text) => handle_add_item(&self, text),
+            AppListviewEvent::RemoveItem() => handle_remove_item(&self), // Add more event handling here as needed
         }
     }
 }
@@ -83,7 +83,10 @@ fn handle_add_item(app: &AppWindow, text: SharedString) {
 
     // Appends user input and empties TextInput component
     if !text.is_empty() {
-        items_model.push(TaskItem {title: text.into(), checked: false});
+        items_model.push(TaskItem {
+            title: text.into(),
+            checked: false,
+        });
         app.set_input_text(SharedString::new());
     }
 }
