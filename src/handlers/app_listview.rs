5a9bed3e029bc32b3aa9ba1d7d63b670f0660bfc
Chris "C.J." Irwin <97114915+ircjwin@users.noreply.github.com>
2024-08-15 16:00:58 -0700

    Ircjwin applistview item delete (#10)
    * feat: check and delete applistview items
    
    app_listview.rs:
    - Add TaskItem struct import
    - Add enum for RemoveItem
    - Add initialization of AppWindow task_item property
    - Add RemoveItem variant to AppWindow implementation
    - Add task_item vector push to AddItem handler
    - Add RemoveItem handler function
    
    appwindow.slint:
    - Add CheckBox import
    - Add TaskItem struct declaration
    - Add task_item property to AppListView
    - Add CheckBox repetition with task_item iteration
    - Add task_item property to AppWindow
    - Add request-remove-item callback to AppWindow
    - Add HorizontalLayout and Button for removing items
    
    * refactor: remove redundant code
    
    app_listview.rs:
    - Convert `task_items_model` and `task_items_model_rc` to `items_model` and `items_model_rc`
    - Clone `ui_weak` and remove redeclaration
    - Add comments for clarity
    
    appwindow.slint:
    - Remove zombie code
    - Convert `task_items` to `items`
    - Add comments for clarity
    
    * perf: dispatch threads for handlers
    
    app_listview.rs:
    - ui_weak_1 and ui_weak_2 converted to Arc<Mutex<T>>
    - Threads spawned inside each UI callback
    - ui_weak_1 and ui_weak_2 upgraded to strong references and sent to Slint's Event Loop
    
    * refactor to use one arc for weak ui
    
    ---------
    
    Co-authored-by: chris0lsen <chris@chrisolsen.net>

--- src/handlers/app_listview.rs
+++ src/handlers/app_listview.rs
@@ -1,46 +1,110 @@
 use crate::ui_modules::AppWindow; // Import the re-exported AppWindow type
                                   // use crate::ui_events::UIEvent; // Import the UIEvent enum
+use crate::ui_modules::TaskItem;
 use slint::ComponentHandle;
 use slint::Model;
 use slint::ModelRc;
 use slint::SharedString;
 use slint::VecModel;
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 pub enum AppListviewEvent {
     AddItem(SharedString),
+    RemoveItem(),
     // Add more events here as needed
 }
 
 pub fn init(ui: &AppWindow) {
-    let items_model: Rc<VecModel<SharedString>> =
-        Rc::new(VecModel::from(vec!["Hello".into(), "World".into()]));
+    // Initialize AppWindow items with placeholder values
+    let items_model: Rc<VecModel<TaskItem>> = Rc::new(VecModel::from(vec![
+        TaskItem {
+            title: "Hello".into(),
+            checked: false,
+        },
+        TaskItem {
+            title: "World".into(),
+            checked: false,
+        },
+    ]));
     let items_model_rc = ModelRc::from(items_model.clone());
     ui.set_items(items_model_rc);
-    let ui_weak = ui.as_weak();
+
+    // Initialize Arc to be cloned by each handler
+    let ui_arc = Arc::new(Mutex::new(ui.as_weak()));
+
+    let add_item_handler_arc = Arc::clone(&ui_arc);
     ui.on_request_add_item(move || {
-        let app = ui_weak.unwrap();
-        let input_text = app.get_input_text();
-        app.handle_event(AppListviewEvent::AddItem(input_text));
+        // Clone Arc for thread
+        let local_handler_clone = Arc::clone(&add_item_handler_arc);
+        thread::spawn(move || {
+            // Locks Weak UI reference until it goes out of scope
+            let ui_weak = local_handler_clone.lock().unwrap();
+            // Capture unused Result in placeholder variable
+            let _ = ui_weak.upgrade_in_event_loop(move |ui| {
+                let text = ui.get_input_text();
+                ui.handle_event(AppListviewEvent::AddItem(text));
+            });
+        });
+    });
+
+    let remove_item_handler_arc = Arc::clone(&ui_arc);
+    ui.on_request_remove_item(move || {
+        // Clone Arc for thread
+        let local_handler_clone = Arc::clone(&remove_item_handler_arc);
+        thread::spawn(move || {
+            // Locks Weak UI reference until it goes out of scope
+            let ui_weak = local_handler_clone.lock().unwrap();
+            // Capture unused Result in placeholder variable
+            let _ = ui_weak.upgrade_in_event_loop(move |ui| {
+                ui.handle_event(AppListviewEvent::RemoveItem());
+            });
+        });
     });
 }
 
 impl AppWindow {
     pub fn handle_event(&self, event: AppListviewEvent) {
         match event {
-            AppListviewEvent::AddItem(text) => handle_add_item(&self, text), // Add more event handling here as needed
+            AppListviewEvent::AddItem(text) => handle_add_item(&self, text),
+            AppListviewEvent::RemoveItem() => handle_remove_item(&self), // Add more event handling here as needed
         }
     }
 }
 
 fn handle_add_item(app: &AppWindow, text: SharedString) {
+    // Convert ModelRc to Model for access to Vector methods
     let items_model_rc = app.get_items();
     let items_model = items_model_rc
         .as_any()
-        .downcast_ref::<VecModel<SharedString>>()
+        .downcast_ref::<VecModel<TaskItem>>()
         .expect("We know we set a VecModel earlier");
+
+    // Appends user input and empties TextInput component
     if !text.is_empty() {
-        items_model.push(SharedString::from(text));
+        items_model.push(TaskItem {
+            title: text.into(),
+            checked: false,
+        });
         app.set_input_text(SharedString::new());
+    }
+}
+
+fn handle_remove_item(app: &AppWindow) {
+    // Convert ModelRc to Model for access to Vector methods
+    let items_model_rc = app.get_items();
+    let items_model = items_model_rc
+        .as_any()
+        .downcast_ref::<VecModel<TaskItem>>()
+        .expect("We know we set a VecModel earlier");
+
+    // Removes checked items and adjusts offset to maintain index order
+    let mut offset = 0;
+    for i in 0..items_model.row_count() {
+        if items_model.row_data(i - offset).unwrap().checked {
+            items_model.remove(i - offset);
+            offset += 1;
+        }
     }
 }
