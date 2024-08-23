f3183e986299ce2a6907d83fc9fd3241baca8fd2
Chris Olsen <chris@chrisolsen.net>
2024-08-23 12:09:04 -0500

    Colsen/single worker thread test (#11)
    * WIP
    
    * rename remove item action
    
    * fix: item removal
    
    appwindow.slint:
    - ListViewItem exposes Checkbox `toggled` callback to `AppListView`
    
    app_listview.rs:
    - print statement modifies index with offset to stay in bounds
    
    * rm refs to save item
    
    * move worker thread to main
    
    * clone tx for app listview
    
    ---------
    
    Co-authored-by: ircjwin <christopherjamesirwin@gmail.com>

--- src/handlers/app_listview.rs
+++ src/handlers/app_listview.rs
@@ -1,22 +1,18 @@
 use crate::ui_modules::AppWindow; // Import the re-exported AppWindow type
                                   // use crate::ui_events::UIEvent; // Import the UIEvent enum
 use crate::ui_modules::TaskItem;
+use crate::UIEvent;
 use slint::ComponentHandle;
 use slint::Model;
 use slint::ModelRc;
 use slint::SharedString;
 use slint::VecModel;
+use slint::Weak;
 use std::rc::Rc;
+use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
-use std::thread;
 
-pub enum AppListviewEvent {
-    AddItem(SharedString),
-    RemoveItem(),
-    // Add more events here as needed
-}
-
-pub fn init(ui: &AppWindow) {
+pub fn init(ui: &AppWindow, tx: Sender<(Arc<Mutex<Weak<AppWindow>>>, UIEvent)>) {
     // Initialize AppWindow items with placeholder values
     let items_model: Rc<VecModel<TaskItem>> = Rc::new(VecModel::from(vec![
         TaskItem {
@@ -35,76 +31,70 @@ pub fn init(ui: &AppWindow) {
     let ui_arc = Arc::new(Mutex::new(ui.as_weak()));
 
     let add_item_handler_arc = Arc::clone(&ui_arc);
+    let add_item_tx = tx.clone();
     ui.on_request_add_item(move || {
         // Clone Arc for thread
         let local_handler_clone = Arc::clone(&add_item_handler_arc);
-        thread::spawn(move || {
-            // Locks Weak UI reference until it goes out of scope
-            let ui_weak = local_handler_clone.lock().unwrap();
-            // Capture unused Result in placeholder variable
-            let _ = ui_weak.upgrade_in_event_loop(move |ui| {
-                let text = ui.get_input_text();
-                ui.handle_event(AppListviewEvent::AddItem(text));
-            });
-        });
+
+        let _ = add_item_tx.send((local_handler_clone, UIEvent::AddItem()));
     });
 
     let remove_item_handler_arc = Arc::clone(&ui_arc);
-    ui.on_request_remove_item(move || {
+    let remove_item_tx = tx.clone();
+    ui.on_request_remove_checked_items(move || {
         // Clone Arc for thread
         let local_handler_clone = Arc::clone(&remove_item_handler_arc);
-        thread::spawn(move || {
-            // Locks Weak UI reference until it goes out of scope
-            let ui_weak = local_handler_clone.lock().unwrap();
-            // Capture unused Result in placeholder variable
-            let _ = ui_weak.upgrade_in_event_loop(move |ui| {
-                ui.handle_event(AppListviewEvent::RemoveItem());
-            });
-        });
+
+        let _ = remove_item_tx.send((local_handler_clone, UIEvent::RemoveCheckedItems()));
     });
 }
 
-impl AppWindow {
-    pub fn handle_event(&self, event: AppListviewEvent) {
-        match event {
-            AppListviewEvent::AddItem(text) => handle_add_item(&self, text),
-            AppListviewEvent::RemoveItem() => handle_remove_item(&self), // Add more event handling here as needed
+pub fn handle_add_item(app: Arc<Mutex<Weak<AppWindow>>>) {
+    // Lock app
+    let app_weak = app.lock().unwrap();
+
+    let _ = app_weak.upgrade_in_event_loop(move |ui| {
+        // Convert ModelRc to Model for access to Vector methods
+        let items_model_rc = ui.get_items();
+        let items_model = items_model_rc
+            .as_any()
+            .downcast_ref::<VecModel<TaskItem>>()
+            .expect("We know we set a VecModel earlier");
+
+        let text = ui.get_input_text();
+
+        // Appends user input and empties TextInput component
+        if !text.is_empty() {
+            items_model.push(TaskItem {
+                title: text.into(),
+                checked: false,
+            });
+            ui.set_input_text(SharedString::new());
         }
-    }
+    });
 }
 
-fn handle_add_item(app: &AppWindow, text: SharedString) {
-    // Convert ModelRc to Model for access to Vector methods
-    let items_model_rc = app.get_items();
-    let items_model = items_model_rc
-        .as_any()
-        .downcast_ref::<VecModel<TaskItem>>()
-        .expect("We know we set a VecModel earlier");
+pub fn handle_remove_checked_items(app: Arc<Mutex<Weak<AppWindow>>>) {
+    // Lock app
+    let app_weak = app.lock().unwrap();
 
-    // Appends user input and empties TextInput component
-    if !text.is_empty() {
-        items_model.push(TaskItem {
-            title: text.into(),
-            checked: false,
-        });
-        app.set_input_text(SharedString::new());
-    }
-}
+    let _ = app_weak.upgrade_in_event_loop(move |ui| {
+        // Convert ModelRc to Model for access to Vector methods
+        let items_model_rc = ui.get_items();
+        let items_model = items_model_rc
+            .as_any()
+            .downcast_ref::<VecModel<TaskItem>>()
+            .expect("We know we set a VecModel earlier");
 
-fn handle_remove_item(app: &AppWindow) {
-    // Convert ModelRc to Model for access to Vector methods
-    let items_model_rc = app.get_items();
-    let items_model = items_model_rc
-        .as_any()
-        .downcast_ref::<VecModel<TaskItem>>()
-        .expect("We know we set a VecModel earlier");
+        // Removes checked items and adjusts offset to maintain index order
+        let mut offset = 0;
+        for i in 0..items_model.row_count() {
+            println!("{:?}", items_model.row_data(i - offset).unwrap());
 
-    // Removes checked items and adjusts offset to maintain index order
-    let mut offset = 0;
-    for i in 0..items_model.row_count() {
-        if items_model.row_data(i - offset).unwrap().checked {
-            items_model.remove(i - offset);
-            offset += 1;
+            if items_model.row_data(i - offset).unwrap().checked {
+                items_model.remove(i - offset);
+                offset += 1;
+            }
         }
-    }
+    });
 }
