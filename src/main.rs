b83ff3ad44e34a721df9e5c9744e6b4bc6adaf78
Chris "C.J." Irwin <97114915+ircjwin@users.noreply.github.com>
2024-09-01 16:49:24 -0700

    Colsen/load data from json (#15) (#16)
    * Read data from json, add config file for data location
    
    * numerous compiler error fixes :)
    
    * fmt
    
    * fmt
    
    Co-authored-by: Chris Olsen <chris@chrisolsen.net>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod data_structs;
 mod events;
 mod handlers;
+mod io;
 mod ui_modules;
 use slint::ComponentHandle;
 use slint::Weak;
+use std::error::Error;
 use std::sync::{mpsc, Arc, Mutex};
 
 use crate::events::*;
@@ -11,16 +14,19 @@ use crate::handlers::app_tabwidget;
 use crate::handlers::ui_worker;
 use crate::ui_modules::AppWindow; // Use the re-exported AppWindow type
 
-fn main() -> Result<(), slint::PlatformError> {
+fn main() -> Result<(), Box<dyn Error>> {
     type Message = (Arc<Mutex<Weak<AppWindow>>>, UIEvent);
     let (tx, rx): (mpsc::Sender<Message>, mpsc::Receiver<Message>) = mpsc::channel();
     let listview_tx = tx.clone();
     let tabwidget_tx = tx.clone();
 
+    // Load from disk
+    let saved_data = io::load()?;
     let ui = AppWindow::new()?;
     ui_worker::init(rx);
     app_listview::init(&ui, listview_tx);
-    app_tabwidget::init(&ui, tabwidget_tx);
+    app_tabwidget::init(&ui, tabwidget_tx, saved_data);
 
-    ui.run()
+    let _ = ui.run();
+    Ok(())
 }
